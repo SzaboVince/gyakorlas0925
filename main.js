379a250a7e684c7661b89a3af10043c33d11023d
Petrik Diak <diak@petrik.hu>
2023-09-25 18:14:10 +0200

    javascript

--- main.js
+++ main.js
@@ -1,2 +1,37 @@
 import './style.css'
 
+document.addEventListener("DOMContentLoaded", init);
+
+function init(){
+    const szamitGomb=document.getElementById("szamit");
+    szamitGomb.addEventListener("click",teruletkerulet);
+}
+
+function teruletkerulet(){
+    const a=parseFloat(document.getElementById("aOldal").value)
+    const b=parseFloat(document.getElementById("bOldal").value)
+    const c=parseFloat(document.getElementById("cOldal").value)
+    const eredmeny=document.getElementById("eredmeny")
+    if(a+b<=c||a+c<=b||b+c<=a){
+        eredmeny.textContent="Ilyen háromszög nem létezik."
+    }
+    else{
+        const haromszog={
+            a:a,
+            b:b,
+            c:c,
+            kerulet: function(){
+                return this.a+this.b+this.c;
+            },
+            terulet: function(){
+                const s=this.kerulet()/2
+                return Math.sqrt(s*(s-this.a)*(s-this.b)*(s-this.c))
+            },
+            keruletesterulet:function(){
+                return `Kerület: ${this.kerulet()} cm - Terület: ${this.terulet()} cm2`
+            }
+        }
+        console.log(haromszog)
+        eredmeny.textContent=haromszog.keruletesterulet();
+    }
+}
